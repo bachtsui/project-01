d2089131f26a476b20b3bb35f3b129811444a2ac
bachtsui <dbtsui2@gmail.com>
2015-12-13 15:12:13 -0800

    Added my models to my index.js

--- models/index.js
+++ models/index.js
@@ -1,2 +1,7 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/project01");
+
+module.exports.Pic = require("./image.js");
+module.exports.Quote = require ("./quote.js");
+module.exports.hairStyle = require("./hairstyle.js");
+
