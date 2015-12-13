33f846bc760f4c730fb6ceb80a5743832f27b9d0
bachtsui <dbtsui2@gmail.com>
2015-12-13 15:35:44 -0800

    Populated my seed.js with some hard coded data

--- seed.js
+++ seed.js
@@ -1,1 +1,20 @@
-// var db = require("./models");
+//Remember seed.js populates our database
+//Don't forget to run nodemon seed.js for checks
+
+var db = require("./models"); //var db is importing everything models is exporting out I think
+
+var hairStyle = [];
+
+hairStyle.push({
+	name: "ASDF123",
+	growthTime:"3 Months",
+	description: "Noticeable by pointyness"
+});
+
+hairStyle.push({
+	name: "QWERTY456",
+	growthTime:"6 Months",
+	description: "Justin wears this beard"
+});
+
+var sampleQuotes = [];
