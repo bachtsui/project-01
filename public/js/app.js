a1297bf17c56db7e22a8c8383a18ac18d9f95950
bachtsui <dbtsui2@gmail.com>
2015-12-14 18:43:36 -0800

    Fixed up placement, delete button is a child of hairstyle box now

--- public/js/app.js
+++ public/js/app.js
@@ -11,21 +11,6 @@ $(document).ready(function(){
       response.forEach(function(hairstyle) {
         renderHairStyle(hairstyle);
       });
-
-  		// for(var i = 0; i < response.length; i++){
-  			// console.log(response[i].name);
-  			// console.log(response[i].growthTime);
-  			// console.log(response[i].description);
-
-        // $("#hairStyle").append("<div class = hair-style-id" + response[i]._id + "</div>");
-        // $("#hairStyle").append("<p> HairStyleID: " + response[i]._id + "</p>");
-  			// $("#hairStyle").append("<p> Name: " + response[i].name + "</p>");
-  			// $("#hairStyle").append("<p> Growth Time: " + response[i].growthTime + "</p>");
-  			// $("#hairStyle").append("<p> Description: " + response[i].description + "</p>");
-
-  			// $("#hairStyle").append("<button class='btn btn-danger delete-hairstyle'>Delete</button>");
-  			//adding a delete button
-  		// }
   	},
 
   	error: function(){
@@ -58,7 +43,7 @@ $(document).ready(function(){
 	});
 
 	$(this).trigger("reset");
-	// //reset the form
+	//reset the form
   });
 
   //DELETE A CURRENT HAIRSTYLING
@@ -67,29 +52,26 @@ $(document).ready(function(){
   //#hairStyle exists and is always listening
   //.delete-hairstyle is created already and now can be clicked on
   console.log("You pressed the delete button");
-  var hairStyleID = $(this).data("id");
-  console.log(hairStyleID);
+  var hairStyleID = $(this).data.hairstyle_.id;
+  console.log("HSD: " , hairStyleID);
         //write body to check for ID
   });
+});
 
-  //We'll use this function to create one hairstyle entry on the page
-  function renderHairStyle (hairstyle){
-    console.log("rendering hairstyle: ", hairstyle);
+//We'll use this function to create one hairstyle entry on the page
+function renderHairStyle (hairstyle){
+  console.log("rendering hairstyle: ", hairstyle);
 
-    var hairstyleHtml =
-    "<!-- One Hairstyle Entry -->" +
+  var hairstyleHtml =
+  "<!-- One Hairstyle Entry -->" +
+  "<div class = 'hairstyle-box' data-hairstyle-id = '" + hairstyle._id + "'>" +
     "<span class = 'hair-style-id'> <p> HairstyleID: " + hairstyle._id + "</p></span>" +
     "<span class = 'hairstyle-name'> <p> Name: " + hairstyle.name + "</p></span>" +
     "<span class = 'hairstyle-growthtime'> <p> Growth Time: " + hairstyle.growthTime + "</p></span>" +
     "<span class = 'hairstyle-description'> <p> Description: " + hairstyle.description + "</p></span>" +
-    "<!-- end One Hairstyle Entry -->";
+    "<span class = 'hairstyle-delete-btn'> <button class='btn btn-danger delete-hairstyle'>Delete</button></span>" +
+  "</div>" +
+  "<!-- end One Hairstyle Entry -->";
 
-    $("#hairStyle").append(hairstyleHtml);
-    $("#hairStyle").append("<button class='btn btn-danger delete-hairstyle'>Delete</button>");
-  }
-
-
-
-
-
-});
+  $("#hairStyle").append(hairstyleHtml);
+}
