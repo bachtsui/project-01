3bebbcf996faa634a364e7df649437883b62c4cc
bachtsui <dbtsui2@gmail.com>
2015-12-17 11:46:49 -0800

    ready for heroku deploy attemp #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/project01");
+//mongoose.connect("mongodb://localhost/project01");
+mongoose.connect(process.env.MONGOLAB_URI ||
+                 process.env.MONGOHQ_URL || 
+                 "mongodb://localhost/project01");
 
 module.exports.Pic = require("./image.js");
 module.exports.Quote = require ("./quote.js");
